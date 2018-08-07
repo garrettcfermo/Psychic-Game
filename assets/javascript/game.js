7b5df1246c3fcd6b4cf4f688b7b578dfdc86a483
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-07 10:48:08 -0700

    JavaScript Guesses Left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,43 @@
 
 
+// The starting points of Scores
+let winScore = 0
+let lossesScore = 0
 
-document.onkeyup = function(event){
+// Possible choices
+const letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+// Number of Guesses User has
+let guessleft = 10
+
+
+document.onkeyup = function (event) {
+
+  // Your Choice
+  userChoice = event.key
+
+  // Decreases guessleft after guess is made
+  if(guessleft >1 && guessleft <=10){
+    guessleft -=1
+  } else if (guessleft=== 1) {
+    guessleft +=9
+  }
+    
+
+  // Computer Choice
+  compChoice = letterChoices[Math.floor(Math.random() * 26)]
 
 
 
 
   // Create a piece of html to display the game's results
-let html =`
-<h3>Wins : ${"hotdog"}</h3>
-<h3>Losses : ${"hotdog"}</h3>
-<h3>Guesses Left : ${"hotdog"}</h3>
-<h3>Your Guesses so far : ${"hotdog"}</h3>
+  let html = `
+<h3>Wins : ${winScore}</h3>
+<h3>Losses : ${lossesScore}</h3>
+<h3>Guesses Left : ${guessleft}</h3>
+<h3>Your Guesses so far : ${userChoice}</h3>
 `
-// Puts the html into the game div
-document.querySelector(".game").innerHTML=html
+  // Puts the html into the game div
+  document.querySelector(".game").innerHTML = html
+
 }
