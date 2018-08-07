4b54ff7385c44f356189dfc699fd9860c2338d3e
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-07 11:05:46 -0700

    JavaScript Computer Choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,13 @@ let lossesScore = 0
 // Possible choices
 const letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+// Initial Computer Choice
+let compChoice = letterChoices[Math.floor(Math.random() * 26)]
+
 // Number of Guesses User has
 let guessleft = 10
 
-// GuessesMade Array
+// Initial GuessesMade Array
 let guessesArr = []
 
 document.onkeyup = function (event) {
@@ -24,21 +27,18 @@ document.onkeyup = function (event) {
   } else if (guessleft=== 1) {
     guessleft +=9
     guessesArr =[]
+    compChoice = letterChoices[Math.floor(Math.random() * 26)]
   }
     
 
-  // Computer Choice
-  compChoice = letterChoices[Math.floor(Math.random() * 26)]
-
-
-
-
+  
   // Create a piece of html to display the game's results
   let html = `
 <h3>Wins : ${winScore}</h3>
 <h3>Losses : ${lossesScore}</h3>
 <h3>Guesses Left : ${guessleft}</h3>
 <h3>Your Guesses so far : ${guessesArr}</h3>
+<h3> Computer Choice: ${compChoice}</h3>
 `
   // Puts the html into the game div
   document.querySelector(".game").innerHTML = html
