9b6f5af94ffbe9e5577adc174dee39b74924df75
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-06 21:33:34 -0700

    JavaScript HTML Output Placeholder

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+
+document.onkeyup = function(event){
+
+
+
+
+  // Create a piece of html to display the game's results
+let html =`
+<h3>Wins : ${"hotdog"}</h3>
+<h3>Losses : ${"hotdog"}</h3>
+<h3>Guesses Left : ${"hotdog"}</h3>
+<h3>Your Guesses so far : ${"hotdog"}</h3>
+`
+// Puts the html into the game div
+document.querySelector(".game").innerHTML=html
+}
