6902f032ae9137824102cfda11c791eb98e27af6
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-07 10:55:57 -0700

    JavaScript Guesses Made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,20 @@ const letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l
 // Number of Guesses User has
 let guessleft = 10
 
+// GuessesMade Array
+let guessesArr = []
 
 document.onkeyup = function (event) {
-
   // Your Choice
   userChoice = event.key
-
+  
   // Decreases guessleft after guess is made
   if(guessleft >1 && guessleft <=10){
     guessleft -=1
+    guessesArr.push(userChoice)
   } else if (guessleft=== 1) {
     guessleft +=9
+    guessesArr =[]
   }
     
 
@@ -35,7 +38,7 @@ document.onkeyup = function (event) {
 <h3>Wins : ${winScore}</h3>
 <h3>Losses : ${lossesScore}</h3>
 <h3>Guesses Left : ${guessleft}</h3>
-<h3>Your Guesses so far : ${userChoice}</h3>
+<h3>Your Guesses so far : ${guessesArr}</h3>
 `
   // Puts the html into the game div
   document.querySelector(".game").innerHTML = html
